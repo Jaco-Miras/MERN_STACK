3ab19f93c7f73ce2265055a8e1e4039297734e34
Jaco-Miras <miras@gmail.com>
2020-07-28 17:59:50 +0800

    ExpressJS

---
+++ ExpressJS/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Paul Perez',
+        email: 'jperez@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Mckeen Asma',
+        email: 'mckeenasma@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Jaco Miras',
+        email: 'miras.jaco@gmail.com',
+        status: 'active'
+    },
+];
+
+module.exports = members;
